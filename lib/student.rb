876fb4db656f9022229310e0285e26ed1ee0fba8
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-29 16:22:01 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+    def learn(knowledge)
+        @knowledge << "Ruby framework Rails gem bundle update"
+    end
+
+    def knowledge
+        "Javascript Ember Elixir knowledge"
+        
+    end
+
 
 end
